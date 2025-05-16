3f07cce6bae55e51bfd20656a533b774cd4d06f2
Jonas <158565390+Jonas13-01@users.noreply.github.com>
2025-05-16 08:28:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,13 @@
 document.getElementById('dataForm').addEventListener('submit', function(event) {
     event.preventDefault(); // Verhindert das Standardverhalten des Formulars (z.B. Neuladen der Seite)
     
-    const lang = document.getElementById('lang').value; // Wert des Sprachfelds holen
-    generateFakeData(lang); // Generiere Fake-Daten mit der ausgewählten Sprache
+
+    generateFakeData(); // Generiere Fake-Daten mit der ausgewählten Sprache
 });
 
 // Funktion zur Generierung von Fake-Daten
-function generateFakeData(lang) {
-    fetch(`https://fakerapi.it/api/v1/persons?_locale=${lang}&_quantity=1`)
+function generateFakeData() {
+    fetch(`https://fakerapi.it/api/v1/persons?_locale=en&_quantity=1`)
         .then(response => response.json())
         .then(data => {
             const person = data.data[0];
@@ -32,8 +32,8 @@ function displayResults(name, birthday, address, email) {
     const results = document.getElementById('results');
     results.innerHTML = `
         <p><strong>Name:</strong> ${name}</p>
-        <p><strong>Geburtsdatum:</strong> ${birthday}</p>
-        <p><strong>Adresse:</strong> ${address}</p>
+        <p><strong>Date of Birth:</strong> ${birthday}</p>
+        <p><strong>Adress:</strong> ${address}</p>
         <p><strong>E-Mail:</strong> ${email}</p>
     `;
 }
